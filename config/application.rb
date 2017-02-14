48bc1a6a3d9caa7f18607c583f1fb4f1dd6d7942
Gavin Li <gavin.li1986@gmail.com>
2017-02-14 14:48:28 +0800

    Config rspec & factory_girl

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,15 @@ module Synced
     config.generators do |g|
       g.helper false
       g.assets false
+      g.template_engine nil
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: true,
+        controller_specs: true,
+        request_specs: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
 end
