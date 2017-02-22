8730ac25aee6c312f8d96334f53eb91e09a3a0ec
Gavin Li <gavin.li1986@gmail.com>
2017-02-22 16:30:06 +0800

    Revert "set some metas"
    This reverts commit e38e9a6119c546c6a2a98d961d2339219969e14b.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :app_meta
   protect_from_forgery with: :exception
-
-  def app_meta
-    site = '机器之心'
-    description = '机器之心全球机器智能峰会'
-    set_meta_tags charset: 'UTF-8'
-    set_meta_tags site: site
-    set_meta_tags viewport: 'initial-scale=1.0,maximum-scale=1.0,width=device-width'
-    set_meta_tags renderer: 'webkit|ie-comp'
-    set_meta_tags description: description
-    set_meta_tags keywords: ['Gmis', '机器之心', '全球机器智能峰会']
-    set_meta_tags reverse: true
-    set_meta_tags og: {
-      url: request.original_url,
-      description: description
-    }
-    set_meta_tags twitter: {
-      url: request.original_url,
-      description: description
-    }
-  end
 end
