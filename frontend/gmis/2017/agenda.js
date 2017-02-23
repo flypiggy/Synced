1d35d5127dd53fb80f3526343526c1806cae509e
Cedcn <cedcn@qq.com>
2017-02-23 14:16:20 +0800

    Responsive - agenda

--- frontend/gmis/2017/agenda.js
+++ frontend/gmis/2017/agenda.js
@@ -1,9 +1,15 @@
 import $ from 'jquery';
 import Swiper from 'swiper';
+import { isMobileUA } from 'mdetect';
 import 'swiper/dist/css/swiper.css';
-const $agendaDays = $('.js-agenda-days');
 
+import wavyLine from '../shared/wavyLine';
+
+const $agendaDays = $('.js-agenda-days');
 const agenda = () => {
+  if (isMobileUA()) return;
+
+  wavyLine();
   new Swiper('#agenda-container', {
     nextButton: '.agenda-button-next',
     prevButton: '.agenda-button-prev',
--- frontend/gmis/gmis2017.js
+++ frontend/gmis/gmis2017.js
@@ -1,17 +1,12 @@
 import $ from 'jquery';
 import 'navsync/dist/navsync.min';
 
-// shared
-import wavyLine from './shared/wavyLine';
-
 import header from './2017/header';
 import agenda from './2017/agenda';
 import address from './2017/address';
 
-
 $(() => {
   header();
-  wavyLine();
   agenda();
   address();
   $('#gmis-nav').navSync({
