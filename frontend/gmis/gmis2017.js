fdb08c8b54c9dd89e4d78f210a615e3abcdfe091
Cedcn <cedcn@qq.com>
2017-02-23 23:45:32 +0800

    Add vote and adjust some images

--- frontend/gmis/gmis2017.js
+++ frontend/gmis/gmis2017.js
@@ -4,10 +4,12 @@ import 'navsync/dist/navsync.min';
 import header from './2017/header';
 import agenda from './2017/agenda';
 import address from './2017/address';
+import vote from './2017/vote';
 
 $(() => {
   header();
   agenda();
+  vote();
   address();
   $('#gmis-nav').navSync({
     offset: 60,
