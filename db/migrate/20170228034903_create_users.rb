46cae0e96ddad4fd1a2dcab022eacc8da963389b
Z Kun <zhukun6150909@outlook.com>
2017-03-01 21:09:55 +0800

    Fix spell bug and enhance code style

--- db/migrate/20170228034903_create_users.rb
+++ db/migrate/20170228034903_create_users.rb
@@ -1,20 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users, id: :uuid, default: 'uuid_generate_v4()' do |t|
-      t.string :email,           unique: true
+      t.string :email,           unique: true, index: true
       t.string :password_digest, null: false, default: ""
 
       t.string :username
-      t.string :mobile,          unique: true
+      t.string :mobile,          unique: true, index: true
       t.string :roles,           array: true, default: []
 
       t.string :city
       t.string :company
       t.string :title
-      t.string :avator
+      t.string :avatar
       t.string :bio
     end
-    add_index :users, :email
-    add_index :users, :mobile
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validation' do
-    context 'when set password no email or mobile' do
-      let(:user_params) { attributes_for(:user, :with_password) }
-      it 'will return error' do
-        user = User.new(user_params)
-        user.valid?
-        expect(user.errors[:password]).to include('You need set email or mobile first')
-      end
+    let(:user_params) { attributes_for(:user, :with_password) }
+    it 'when set password no email or mobile will return error' do
+      user = User.new(user_params)
+      user.valid?
+      expect(user.errors[:password]).to include('You need set email or mobile first')
     end
   end
 end
