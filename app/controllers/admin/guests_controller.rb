da4780026aa84acb1dc0b4cb22eb340ed828ed4c
Z Kun <zhukun6150909@outlook.com>
2017-03-02 13:33:19 +0800

    Finish guest CRUD

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -8,7 +8,8 @@ class Admin::GuestsController < Admin::BaseController
   end
 
   def create
-    build_guest
+    build_guest_with_params
+    save_guest
   end
 
   def edit
@@ -17,24 +18,58 @@ class Admin::GuestsController < Admin::BaseController
 
   def update
     load_guest
-    build_guest
+    update_guest
   end
 
   def destroy
     load_guest
+    destroy_guest
   end
 
   private
 
   def load_guests
-    @guests = Guest.all.page(params[:page]).per(10)
+    @guests = Guest.all.order(:created_at).page(params[:page]).per(10)
   end
 
   def load_guest
     @guest = Guest.find(params[:id])
   end
 
-  def build_guest; end
+  def build_guest
+    @guest = Guest.new
+  end
 
-  def guest_params; end
+  def build_guest_with_params
+    @guest = Guest.new(guest_params)
+  end
+
+  def update_guest
+    if @guest.update(guest_params)
+      flash.now[:notice] = '更新成功'
+    else
+      flash.now[:error] = @guest.errors.full_messages
+    end
+    render :edit
+  end
+
+  def save_guest
+    if @guest.save
+      flash[:notice] = '创建成功'
+      redirect_to params[:continue] ? new_admin_guest_path : admin_guests_path
+    else
+      flash.now[:error] = @guest.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy_guest
+    @guest.destroy
+    flash[:notice] = '删除成功'
+    redirect_to admin_guests_path
+  end
+
+  def guest_params
+    params.require(:guest).permit(:name, :company, :title)
+  end
 end
--- spec/controllers/admin/guests_controller_spec.rb
+++ spec/controllers/admin/guests_controller_spec.rb
@@ -8,5 +8,31 @@ RSpec.describe Admin::GuestsController, type: :controller do
         expect(response).to be_success
       end
     end
+
+    describe 'Get #new' do
+      it 'should be ok' do
+        get :new
+        expect(response).to be_success
+      end
+    end
+
+    describe 'Post #create' do
+      let(:guest_params) { attributes_for(:guest) }
+      let(:invalid_guest_params) { attributes_for(:guest, name: '') }
+      it 'return error if params invalid' do
+        post :create, params: { guest: guest_params }
+        expect(response).to redirect_to(admin_guests_path)
+      end
+
+      it 'create a new guest if params valid' do
+        post :create, params: { guest: invalid_guest_params }
+        expect(flash[:error]).to include('Name不能为空字符')
+      end
+
+      it 'direct to new if params has continue' do
+        post :create, params: { guest: guest_params, continue: 'anything' }
+        expect(response).to redirect_to(new_admin_guest_path)
+      end
+    end
   end
 end
