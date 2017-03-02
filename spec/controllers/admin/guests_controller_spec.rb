966775f06688b3b017beae5f4837de699f2c8347
Z Kun <zhukun6150909@outlook.com>
2017-03-02 08:37:01 +0800

    Add guest index page

--- spec/controllers/admin/guests_controller_spec.rb
+++ spec/controllers/admin/guests_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Admin::GuestsController, type: :controller do
+  describe 'CRUD' do
+    describe 'Get #index' do
+      it 'return 200' do
+        get :index
+        expect(response).to be_success
+      end
+    end
+  end
 end
