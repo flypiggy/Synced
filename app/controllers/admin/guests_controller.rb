ce478297f647f3c52b626da4c6bb50e75519a984
Z Kun <zhukun6150909@outlook.com>
2017-03-03 15:07:14 +0800

    Use simple form

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -4,12 +4,16 @@ class Admin::GuestsController < Admin::BaseController
   end
 
   def new
-    build_guest
+    @guest = Guest.new
   end
 
   def create
-    build_guest_with_params
-    save_guest
+    @guest = Guest.new(guest_params)
+    if @guest.save
+      redirect_to params[:continue] ? new_admin_guest_path : admin_guests_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,55 +22,24 @@ class Admin::GuestsController < Admin::BaseController
 
   def update
     load_guest
-    update_guest
+    @guest.update(guest_params)
+    render :edit
   end
 
   def destroy
     load_guest
-    destroy_guest
+    @guest.destroy
+    redirect_to admin_guests_path
   end
 
   private
 
   def load_guests
-    @guests = Guest.all.order(:created_at).page(params[:page]).per(10)
+    @guests = Guest.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def load_guest
     @guest = Guest.find(params[:id])
-  end
-
-  def build_guest
-    @guest = Guest.new
-  end
-
-  def build_guest_with_params
-    @guest = Guest.new(guest_params)
-  end
-
-  def update_guest
-    if @guest.update(guest_params)
-      flash.now[:notice] = '更新成功'
-    else
-      flash.now[:error] = @guest.errors.full_messages
-    end
-    render :edit
-  end
-
-  def save_guest
-    if @guest.save
-      flash[:notice] = '创建成功'
-      redirect_to params[:continue] ? new_admin_guest_path : admin_guests_path
-    else
-      flash.now[:error] = @guest.errors.full_messages
-      render :new
-    end
-  end
-
-  def destroy_guest
-    @guest.destroy
-    flash[:notice] = '删除成功'
-    redirect_to admin_guests_path
   end
 
   def guest_params
--- spec/controllers/admin/guests_controller_spec.rb
+++ spec/controllers/admin/guests_controller_spec.rb
@@ -19,14 +19,14 @@ RSpec.describe Admin::GuestsController, type: :controller do
     describe 'Post #create' do
       let(:guest_params) { attributes_for(:guest) }
       let(:invalid_guest_params) { attributes_for(:guest, name: '') }
-      it 'return error if params invalid' do
+      it 'redirect if params valid' do
         post :create, params: { guest: guest_params }
         expect(response).to redirect_to(admin_guests_path)
       end
 
-      it 'create a new guest if params valid' do
+      it 'render edit if params invalid' do
         post :create, params: { guest: invalid_guest_params }
-        expect(flash[:error]).to include('Name不能为空字符')
+        expect(response.status).to eq 200
       end
 
       it 'direct to new if params has continue' do
