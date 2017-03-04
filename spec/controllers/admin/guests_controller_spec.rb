ee28bdf0444e6a4d4a5be768e3c11b73483c7e3d
Z Kun <zhukun6150909@outlook.com>
2017-03-04 13:01:15 +0800

    Use SJR

--- spec/controllers/admin/guests_controller_spec.rb
+++ spec/controllers/admin/guests_controller_spec.rb
@@ -1,38 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Admin::GuestsController, type: :controller do
-  describe 'CRUD' do
-    describe 'Get #index' do
-      it 'return 200' do
-        get :index
-        expect(response).to be_success
-      end
+  describe 'Get #index' do
+    it 'return 200' do
+      get :index
+      expect(response).to be_success
     end
+  end
 
-    describe 'Get #new' do
-      it 'should be ok' do
-        get :new
-        expect(response).to be_success
-      end
+  describe 'Post #create' do
+    let(:guest_params) { attributes_for(:guest) }
+    let(:invalid_guest_params) { attributes_for(:guest, name: '') }
+    it 'create a guest if params valid' do
+      expect { post :create, params: { guest: guest_params }, format: 'js' }.to change { Guest.count }.by(1)
     end
+  end
 
-    describe 'Post #create' do
-      let(:guest_params) { attributes_for(:guest) }
-      let(:invalid_guest_params) { attributes_for(:guest, name: '') }
-      it 'redirect if params valid' do
-        post :create, params: { guest: guest_params }
-        expect(response).to redirect_to(admin_guests_path)
-      end
+  describe 'Patch #update' do
+    let(:guest) { create(:guest) }
+    it 'change name when update guest name' do
+      patch :update, params: { guest: { name: 'changed_name' }, id: guest.id }, format: 'js'
+      expect(guest.reload.name).to eq 'changed_name'
+    end
+  end
 
-      it 'render edit if params invalid' do
-        post :create, params: { guest: invalid_guest_params }
-        expect(response.status).to eq 200
-      end
-
-      it 'direct to new if params has continue' do
-        post :create, params: { guest: guest_params, continue: 'anything' }
-        expect(response).to redirect_to(new_admin_guest_path)
-      end
+  describe 'Delete #destroy' do
+    let(:guest) { create(:guest) }
+    it 'will destroy this gust when delete request to destroy' do
+      delete :destroy, params: { id: guest.id }, format: 'js'
+      expect(Guest.find_by(id: guest.id)).to eq nil
     end
   end
 end
