b5cf4b406909e4aa0e3abb3e6015e1ba96ca71c3
Cedcn <cedcn@qq.com>
2017-03-10 00:10:43 +0800

    Modify spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe VotesController, type: :controller do
     end
 
     it 'should success vote enabled' do
-      put :vote_up, params: { id: vote_item.id, format: :js }
+      put :vote_up, params: { id: vote_item.id, format: :json }
       expect(response).to be_success
     end
 
     it 'should render right count' do
-      3.times { put :vote_up, params: { id: vote_item.id, format: :js } }
+      3.times { put :vote_up, params: { id: vote_item.id, format: :json } }
       expect(JSON.parse(response.body)['total']).to eq 3
     end
   end
