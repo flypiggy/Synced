eadec77647233118e96a2ac16e4cb7e39cf2f1cf
Gavin Li <gavin.li1986@gmail.com>
2017-03-12 00:33:15 +0800

    Remove  origin webpack helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def webpack_include_tag(name)
-    if Rails.configuration.webpack[:manifest]
-      asset_name = Rails.configuration.webpack[:manifest]["#{name}.js"]
-      return unless asset_name.present?
-    end
-    src = cdn_assets_url(name, asset_name)
-    javascript_include_tag src
-  end
-
   def page_id
     controller_names = controller_path.split('/')
     [controller_names, action_name].compact.flatten.join('-')
@@ -19,19 +10,5 @@ module ApplicationHelper
 
   def device_mobile?
     browser.device.ipad? || browser.device.mobile?
-  end
-
-  private
-
-  def cdn_assets_url(name, asset_name)
-    if Rails.env.production?
-      "#{Rails.configuration.asset_host}/static/#{asset_name}"
-    elsif asset_name.present?
-      "/static/#{asset_name}"
-    elsif request.port == 8080
-      "/static/#{name}.js"
-    else
-      "//localhost:8080/assets/#{name}.js"
-    end
   end
 end
