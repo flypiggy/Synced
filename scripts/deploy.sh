9b3ee0586fcab50d8cb107a1f9d3b9312af197cc
Gavin Li <gavin.li1986@gmail.com>
2017-03-13 11:30:36 +0800

    Correct deploy script path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "-> Setting environments"
-openssl aes-256-cbc -K $encrypted_9e7da2809d56_key -iv $encrypted_9e7da2809d56_iv -in ../config/deploy.enc -out ../config/deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_9e7da2809d56_key -iv $encrypted_9e7da2809d56_iv -in config/deploy.enc -out config/deploy_rsa -d
 
 echo "-> Installing bundler gem"
 gem install bundler
