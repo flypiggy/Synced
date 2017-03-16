7f1e09a0f7079a5512233f5eed5835deb8b94f53
Gavin Li <gavin.li1986@gmail.com>
2017-03-16 14:46:51 +0800

    Add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,48 @@
 class User < ApplicationRecord
+  include Pinyinable
+
   has_secure_password validation: false
-  validates_absence_of :password, message: 'You need set email or mobile first',
-    if: -> { email.blank? && mobile.blank? }
+
+  validates :name, length: { in: 2..20 }, presence: true,
+    format: { with: /\A(?!_)(?!.*?_$)[a-zA-Z0-9_\u4e00-\u9fa5]+\z/ }
+
   validates :email,  uniqueness: true,
     format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ },
     allow_nil: true
   validates :mobile, uniqueness: true,
     format: { with: /\A\d{11}\z/, message: 'only 11 numbers china mobile' },
     allow_nil: true
+  validates :mobile, presence: { message: 'email and mobile at least have one' },
+                     on: :create, if: ->(user) { user.email.blank? }
+
+  validates :username, length: { in: 4..20 },
+    format: { with: /\A(?!_)(?!.*?_$)[a-zA-Z0-9_]+\z/ },
+    uniqueness: true, on: :update
   validates :password, length: { in: 6...32 }
-  validates :username, length: { in: 6...20 }
+  validates_absence_of :password, message: 'You need set email or mobile first',
+    if: -> { email.blank? && mobile.blank? }
+
+  before_create :generate_username
 
   class << self
-    def search_by_email_or_mobile(identify)
+    def search_by_login_name(identify)
       q = identify.to_s.downcase.chomp
       return unless q
-      find_by('lower(email) = ? or mobile = ?', q, q)
+      find_by('lower(email) = ? or mobile = ? or username = ?', q, q, q)
     end
+  end
+
+  def authenticate(password)
+    super(password)
+  rescue BCrypt::Errors::InvalidHash
+    nil
+  end
+
+  private
+
+  def generate_username
+    self[:username] = pinyin.slice(0, 20)
+    self[:username] = SecureRandom.hex(10) while User.exists? username: username
   end
 end
 
@@ -35,9 +61,15 @@ end
 #  title           :string
 #  avatar          :string
 #  bio             :string
+#  name            :string
+#  pinyin          :string
+#  pinyin_abbr     :string
 #
 # Indexes
 #
-#  index_users_on_email   (email)
-#  index_users_on_mobile  (mobile)
+#  index_users_on_email        (email)
+#  index_users_on_mobile       (mobile)
+#  index_users_on_pinyin       (pinyin)
+#  index_users_on_pinyin_abbr  (pinyin_abbr)
+#  index_users_on_username     (username)
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309072302) do
+ActiveRecord::Schema.define(version: 20170316032240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,14 @@ ActiveRecord::Schema.define(version: 20170309072302) do
     t.string "title"
     t.string "avatar"
     t.string "bio"
+    t.string "name"
+    t.string "pinyin"
+    t.string "pinyin_abbr"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["mobile"], name: "index_users_on_mobile", using: :btree
+    t.index ["pinyin"], name: "index_users_on_pinyin", using: :btree
+    t.index ["pinyin_abbr"], name: "index_users_on_pinyin_abbr", using: :btree
+    t.index ["username"], name: "index_users_on_username", using: :btree
   end
 
   create_table "vote_items", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
